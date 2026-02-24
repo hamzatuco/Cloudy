f37d88d026e5eaca22d1a55c03e098a63e4228c8
hamzatuco <hamza.tuco@outlook.com>
2026-02-25 00:42:49 +0100

    Added AppStorage for Saving Cities and Checking If User Allowed Location Services

---
+++ Cloudy.App/cloudy/lib/core/storage/app_storage.dart
@@ -0,0 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppStorage {
+  static const _keyHasOnboarded = 'has_onboarded';
+  static const _keyFavoriteCity = 'favorite_city';
+}
